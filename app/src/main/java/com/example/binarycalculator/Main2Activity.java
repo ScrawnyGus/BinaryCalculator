b2712c6055e85c140a66aecd168735f90a65f803
Darma Wiryanata <darmawiryanata@gmail.com>
2019-04-27 16:46:31 +0800

    hosts count

--- app/src/main/java/com/example/binarycalculator/Main2Activity.java
+++ app/src/main/java/com/example/binarycalculator/Main2Activity.java
@@ -7,8 +7,9 @@ import android.widget.TextView;
 import java.util.ArrayList;
 
 public class Main2Activity extends AppCompatActivity {
-    private TextView Oktal1, Oktal2, Oktal3, Oktal4, Subnet;
-    String stOktal1, stOktal2, stOktal3, stOktal4, stSubnet;
+    private TextView Oktal1, Oktal2, Oktal3, Oktal4, Subnet, kelas, hosts;
+    String stOktal1, stOktal2, stOktal3, stOktal4, stSubnet, sthasil;
+    Integer sstOktal1, sstSubnet, kurang, kurang2, hasil;
     Bundle bundle;
     ArrayList<Integer> values;
 
@@ -17,6 +18,7 @@ public class Main2Activity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
 
+//        IP Atas
         Oktal1 = findViewById(R.id.txtOktal1);
         Oktal2 = findViewById(R.id.txtOktal2);
         Oktal3 = findViewById(R.id.txtOktal3);
@@ -35,5 +37,36 @@ public class Main2Activity extends AppCompatActivity {
         Oktal3.setText(String.valueOf(stOktal3));
         Oktal4.setText(String.valueOf(stOktal4));
         Subnet.setText(String.valueOf(stSubnet));
+
+//        Class
+        sstOktal1 = Integer.parseInt(stOktal1);
+        kelas = findViewById(R.id.txtclass);
+        if (sstOktal1<= 126) {
+            kelas.setText("Class A");
+        }
+        else if (sstOktal1>=127 && sstOktal1<=191) {
+            kelas.setText("Class B");
+        }
+        else if (sstOktal1>=192 && sstOktal1<=223) {
+            kelas.setText("Class C");
+        }
+        else if (sstOktal1>=224 && sstOktal1<=239) {
+            kelas.setText("Class D");
+        }
+        else if (sstOktal1>=240 && sstOktal1<=225) {
+            kelas.setText("Class E");
+        }
+
+//        Hosts
+        sstSubnet = Integer.parseInt(stSubnet);
+        hosts = findViewById(R.id.host);
+        kurang = 32 - sstSubnet;
+        hasil = 1;
+        for (int a = 1; a<=kurang; a++){
+            hasil = hasil * 2;
+        }
+        kurang2 = hasil - 2;
+        sthasil = kurang2.toString();
+        hosts.setText(String.valueOf(sthasil));
     }
 }
